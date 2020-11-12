fc1f1370a2f8d7248038ba83c65b334f5451f469
jasmine <1420775889@qq.com>
2020-11-12 09:24:37 +0800

    index和start更新到最新版本

--- wxapp/pages/index/index.js
+++ wxapp/pages/index/index.js
@@ -1,19 +1,22 @@
 //index.js
+var userutil = require('../../utils/userutil.js')
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    dialogShow: false,
+    oneButton: [{ text: '确定' }],
+    items: [
+      { name: '1', value: '男', checked: 'true'},
+      { name: '2', value: '女' }
+    ],
+    date: '2016-09-01',
+    gender :1,
+    
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -43,12 +46,68 @@ Page({
       })
     }
   },
+  openForm: function () {
+    this.setData({
+      dialogShow: true
+    })
+  },
   getUserInfo: function(e) {
-    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+    if(!wx.getStorageSync('userInfo')){
+      userutil.userlogin(app.globalData.userInfo)
+      userutil.userloginCallback = res => {
+        console.log(res.data)
+        if (res.data.flag) {
+          console.log("yes")
+          this.openForm()
+        } else {
+          wx.redirectTo({
+            url: '../start/start'
+          })
+        }
+      }
+    }else{
+      wx.redirectTo({
+        url: '../start/start'
+      })
+    }
+  },
+  tapDialogButton(e) {
+    this.setData({
+      showOneButtonDialog: true
+    }),
+      wx.setStorageSync('userInfo', Object.assign(wx.getStorageSync('userInfo'), { 'birthday': this.data.date, 'gender': this.data.gender, 'avatarUrl':this.data.userInfo.avatarUrl, 'avatarUrl':this.data.userInfo.avatarUrl}))
+    userutil.personalInfo(wx.getStorageSync('userInfo'))
+    wx.redirectTo({
+      url: '../start/start'
+    })
+  },
+  radioChange: function (e) {
+    this.setData({
+      gender: e.detail.value
+    })
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
     })
   }
 })
